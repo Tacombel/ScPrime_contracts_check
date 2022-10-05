2848a94b3ac41d39af5c48089da03f9153cc8c27
Tacombel <tacombel@gmail.com>
2022-10-05 17:11:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
     # the first part of the command you use to call spc
-    hosts = ['scprime01']
+    hosts = ['scprime01'] # This is the name of the container
     base_cmd = 'docker exec'
