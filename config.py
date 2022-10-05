1852ab4e450c77cd2dde09a95320ab7d2b9b6414
Tacombel <tacombel@gmail.com>
2022-10-05 17:15:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config(object):
-    # Thias is for scprime running inside a container. If you host it directly, check line 11 and adapt it to your command
+    # Thias is for scprime running inside a container. If you host it directly, check line 11 and adapt it to your command.
+    # Also, for a single host, remove the for loop at line 64.
     hosts = ['scprime01'] # This is the name of the container
     base_cmd = 'docker exec'
