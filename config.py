dcb9914704c3df12847753eaa56bc0c582ea42ca
Tacombel <tacombel@gmail.com>
2022-10-05 17:13:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    # the first part of the command you use to call spc
+    # Thias is for scprime running inside a container. If you host it directly, check line 11 and adapt it to your command
     hosts = ['scprime01'] # This is the name of the container
     base_cmd = 'docker exec'
